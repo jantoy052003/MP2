6e529d690178d0aa2336e0d56c1992254bb8d4f3
jantoy052003 <124265990+jantoy052003@users.noreply.github.com>
2023-03-29 11:21:59 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigationbar from './components/Navigationbar'
+
+const App = () => {
+  return (
+    <>
+      <Navigationbar/>
+    </>
+  )
+}
+
+export default App
