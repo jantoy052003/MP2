4c76f61592de92fc8a9f478f78503a9f1c0de88e
kd-engineer <durantekevin004@gmail.com>
2023-04-06 14:28:58 +0800

    edit home page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -8,10 +8,10 @@ const Signup = () => {
     <div className='welcome-container'>
         <img src={collageBG} alt="" />
         <div id='header-1' className='d-flex mx-auto'>
-            <img src={Logo} alt="Logo" className='mt-3' />
+            <img id='welcome-logo' src={Logo} alt="Logo" className='mt-3' />
             <Button href='/login' id='btn' className='py-2'>Sign In</Button>
         </div>
-        <div className='d-flex align-items-center justify-content-center'> 
+        <div id='welcome-info' className='d-flex align-items-center justify-content-center'> 
             <div className='d-flex flex-column text-white'>
                 <h1 id='unli' className='text-center'>UNLIMITED MOVIES</h1>
                 <h4 className='text-center'>Watch Anywhere. Cancel Anytime.</h4>
