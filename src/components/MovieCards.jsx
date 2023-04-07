736d069c26b30ec6ac12938ffe2b82b8dd96beae
jantoy052003 <124265990+jantoy052003@users.noreply.github.com>
2023-04-07 12:43:01 +0800

    added popular page and MovieCards component

---
+++ src/components/MovieCards.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+// import Trailer from "../components/Trailer"; //di ko na muna sinama si trailer
+
+
+const MovieCards = ({movie}) => {
+
+  return (
+    <>
+        <Card style={{ width: '20rem' }} className= " cardsContainer mx-3 my-3">
+            <Card.Img  variant="top" src={`https://image.tmdb.org/t/p/w500/`+ movie.poster_path} /> {/**completes the path to the poster */} 
+            <Card.Body>
+                <Card.Title>{movie.original_title}</Card.Title>
+                <Card.Text>
+                    {movie.overview}
+                    {/* <YoutubeEmbed embedId={movie.key} /> */}
+                </Card.Text>
+                {/* <Trailer movie={movie}/> */}
+            </Card.Body>
+        </Card>
+    </>
+  )
+}
+
+export default MovieCards
