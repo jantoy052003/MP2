8cc67a9e05e8d26ca5100ca408c0847452fdf13a
kd-engineer <durantekevin004@gmail.com>
2023-04-11 23:44:54 +0800

    add modal to upcoming movies page

--- src/components/layout/UpcomingMoviesLayout.jsx
+++ src/components/layout/UpcomingMoviesLayout.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
 import '../../style/UpcomingMovies.css'
+import Modal from 'react-bootstrap/Modal'
+import React, { useState } from 'react';
+import YoutubeEmbed from '../YoutubeEmbed';
 
 const UpcomingMoviesLayout = ({movie}) => {
+  const [lgShow, setLgShow] = useState(false);
+
   return (
     <div >
       <div id="backdrop">
-        <img src={`https://www.themoviedb.org/t/p/original`+ movie.backdrop_path} alt="" />
+        <img src={`https://www.themoviedb.org/t/p/original`+ movie.backdrop_path} />
       </div>
       <div id='movie-info'>
-        <img src={`https://www.themoviedb.org/t/p/original/`+ movie.poster_path} alt="" />
+        <img src={`https://www.themoviedb.org/t/p/original/`+ movie.poster_path} onClick={() => setLgShow(true)} />
+          <Modal
+            size="lg"
+            show={lgShow}
+            onHide={() => setLgShow(false)}
+            aria-labelledby="overview"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="overview">
+                {movie.title}
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <YoutubeEmbed embedId={movie.key} />
+            </Modal.Body>
+            <Modal.Footer>
+            <div className='py-3'>
+              {movie.overview}
+            </div>
+          </Modal.Footer>
+        </Modal>
         <div id='info-overview'>
           <h3>{movie.title}</h3>
           <h6>Release Date: {movie.release_date}</h6>
